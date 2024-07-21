8673d607079beef6191d30e6c3ae479178c70865
Ivan Laptop <ivan.teohjh@gmail.com>
2024-07-21 15:47:12 +0800

    Why wont you display

--- frontend/public/scripts/pages/profile.js
+++ frontend/public/scripts/pages/profile.js
@@ -24,12 +24,70 @@ export default function template(prop={}) {
                 <div class="profile-info">
                     <img src="bocchi.jpeg" alt="Profile Picture" class="profile-pic">
                     <h2 class="mt-3">Username</h2>
-                    <div class="game-stats">
-                        <p>Games Played: 3</p>
-                        <p>Games Won: 2</p>
-                        <p>Win/Lost Ratio: 1.5</p>
-                    </div>
                 </div>
+                <div class="chartBox">
+	            	<canvas id="myChart"></canvas>
+                </div>
+                <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js/dist/chart.umd.min.js"></script>
+                <script type="text/javascript">
+                    const data = {
+                        labels: ['Losses', 'Wins'],
+                        datasets: [{
+                            data: [3, 5],
+                            backgroundColor: [
+                                'rgba(255, 26, 104, 0.2)',
+                                'rgba(54, 162, 235, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 26, 104, 1)',
+                                'rgba(54, 162, 235, 1)'
+                            ],
+                            borderWidth: 1,
+                            cutout: '60%'
+                        }]
+                    };
+                    const ratio = {
+                        id: 'ratio',
+                        beforeDatasetsDraw(chart, args, plugins) {
+                            const { data, ctx } = chart;
+                            const xCenter = chart.getDatasetMeta(0).data[0].x;
+                            const yCenter = chart.getDatasetMeta(0).data[0].y;
+                            const ratio = ((data.datasets[0].data[1] / (data.datasets[0].data[1] + data.datasets[0].data[0])) * 100).toFixed(0);
+                            ctx.save();
+                            ctx.translate(xCenter, yCenter);
+                            ctx.font = 'bold 65px sans-serif';
+                            ctx.fillStyle = 'gray';
+                            ctx.textBaseline = 'bottom';
+                            ctx.textAlign = 'center';
+                            ctx.fillText('Won', 0, 0 - 5)
+                            ctx.font = 'bold 80px sans-serif';
+                            ctx.fillStyle = 'gray';
+                            ctx.textBaseline = 'top';
+                            ctx.textAlign = 'center';
+                            ctx.fillText(\`\${ratio}%\`, 0, 0);
+                            ctx.restore();
+                        }
+                    };
+                    const config = {
+                        type: 'doughnut',
+                        data,
+                        options: {
+                            layout: {
+                                padding: 70,
+                            },
+                            plugins: {
+                                legend: {
+                                    display: false,
+                                }
+                            }
+                        },
+                        plugins: [ratio]
+                    };
+                    const myChart = new Chart(
+                        document.getElementById('myChart'),
+                        config
+                    );
+                </script>
                 <div class="col-md-8 match-history ">
                     <h3>Match History</h3>
                     <div class="tab-content mt-3" id="matchHistoryTabContent">
