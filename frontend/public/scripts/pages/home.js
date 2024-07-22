78450a0510879f118a7fd624ac9234f0806d894c
cyu-xian <thg7091@gmail.com>
2024-07-22 18:10:13 +0800

    home+profile done

--- frontend/public/scripts/pages/home.js
+++ frontend/public/scripts/pages/home.js
@@ -1,3 +1,4 @@
+import { redirect } from "../router.js"
 
 export default function home(prop={}) {
 	let prerender = () => {
@@ -27,13 +28,13 @@ export default function home(prop={}) {
 				<div class="d-flex col-md-4 mb-3" id="tournament">
 				<button class="flex-grow-1 ui-btn btn-block"><span>Tournament</span></button>
 				</div>
-				<div class="d-flex col-md-4 mb-3">
+				<div class="d-flex col-md-4 mb-3" id="language">
 				<button class="flex-grow-1 ui-btn btn-block"><span>Language</span></button>
 				</div>
-				<div class="d-flex col-md-4 mb-3">
+				<div class="d-flex col-md-4 mb-3" id="profile">
 				<button class="flex-grow-1 ui-btn btn-block"><span>Profile</span></button>
 				</div>
-				<div class="d-flex col-md-4 mb-3">
+				<div class="d-flex col-md-4 mb-3" id="friends">
 				<button class="flex-grow-1 ui-btn btn-block"><span>Friends....?</span></button>
 				</div>
 			</div> 
@@ -49,6 +50,21 @@ export default function home(prop={}) {
 		const	ApongUsGame = async () => {
 			alert('Entering APONG US game')
 		}
+
+		const	 go_profile = async () => {
+			console.log("clicked");
+			redirect("/profile")
+		}
+
+		const	 go_friends = async () => {
+			redirect("/friendlist")
+		}
+
+		const profile_button = document.getElementById("profile")
+		profile_button.addEventListener('click', go_profile)
+
+		const friend_button = document.getElementById("friends")
+		friend_button.addEventListener('click', go_friends)
 
 		const pong_button = document.getElementById("pongGame")
 		pong_button.addEventListener('click', pongGame)
