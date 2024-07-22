2fa7e6bfd9e76f8bed55839a42d53c5b7707a26d
“Justyn” <justyntkw@gmail.com>
2024-07-22 15:49:15 +0800

    oop

--- backend/src/backend/settings.py
+++ backend/src/backend/settings.py
@@ -191,5 +191,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'justyntkw@gmail.com'
-EMAIL_HOST_PASSWORD = 'j'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
