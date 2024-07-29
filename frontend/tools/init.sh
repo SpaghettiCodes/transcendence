f6e92aa8016b1faa4ce5e878cec5012ac1c34b2c
NotChuaSai <chuashixiang@gmail.com>
2024-07-29 17:03:54 +0800

    minor changes to ssl certi

--- frontend/tools/init.sh
+++ frontend/tools/init.sh
@@ -5,7 +5,7 @@
 # its better to just put it here and let it make a new cert whenever you run it, instead of rebuilding the entire container
 
 echo "Generating SSL Certificate ... "
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=$country/ST=$state/L=$city/O=$org/OU=$position/CN=$domain_name" 2> /dev/null
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=MY/ST=Selangor/L=ShahAlam/O=ApongUs/OU=IDK/CN=apongus.com" 2> /dev/null
 echo "Done!"
 
 echo "Nginx web server is runnning now!"
